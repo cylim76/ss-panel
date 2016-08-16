8028cf2ed72ff76b48e475366cc755bf98701b87
https://github.com/monokoo <realstones2012@gmail.com>
2016-08-16 18:14:05 +0800

    send email to the users who will be expired in the next day

--- app/Command/DailyMail.php
+++ app/Command/DailyMail.php
@@ -27,4 +27,29 @@ class DailyMail
             }
         }
     }
+    
+    public static function sendexpiredMail()
+    {
+        $users = User::all();
+        foreach ($users as $user) {
+          $datenow = date("Y-m-d");
+          $expireday = $user->expire_at;
+          $informday = date("Y-m-d",strtotime("-1 day $expireday"));
+          if ($datenow == $informday){
+            echo "Send account expired mail to user: " . $user->id;
+            $subject = Config::get('appName') . "-您的账户即将到期";
+            $to = $user->email;
+            $text = "您好:<br>&emsp;&emsp;您的账户将于 ".$expireday." 到期，请及时联系管理员，如已经联系处理，请忽略本邮件。谢谢";
+            try {
+                Mail::send($to, $subject, 'news/account-expire-info.tpl', [
+                    "user" => $user,"text" => $text
+                ], [
+                ]);
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+          }
+        }
+    }
+    
 }
